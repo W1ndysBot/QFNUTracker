2e5536bcf3441656147b5481f45c630ff6a26395
W1ndys <w1ndys@outlook.com>
2024-09-07 12:24:38 +0800

    调整menu的位置改成内置

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ sys.path.append(
 )
 
 from app.switch import get_all_group_switches
-
+from app.api import send_group_msg
 from app.scripts.QFNUTracker.jwc_gg_j import (
     start_monitoring as jwc_gg_j_start_monitoring,
     handle_QFNUTracker_group_message as jwc_gg_j_handle_group_message,
@@ -37,6 +37,31 @@ async def start_qfnu_tracker(websocket):
     await zcc_zbgg1_start_monitoring(websocket)
 
 
+# QFNU定制服务
+async def QFNU(websocket, group_id, message_id):
+    message = (
+        f"[CQ:reply,id={message_id}]\n"
+        + """
+QFNU定制服务
+
+曲阜师范大学公告监控
+qfnujwc-on 开启教务处监控
+qfnujwc-off 关闭教务处监控
+qfnuzcc-on 开启资产处监控
+qfnuzcc-off 关闭资产处监控
+更多内容更新中...（鸽）
+
+技术支持：W1ndys
+"""
+    )
+    await send_group_msg(websocket, group_id, message)
+
+
 async def handle_QFNUTracker_group_message(websocket, msg):
     await jwc_gg_j_handle_group_message(websocket, msg)
     await zcc_zbgg_handle_group_message(websocket, msg)
+    raw_message = msg.get("raw_message")
+    group_id = msg.get("group_id")
+    message_id = msg.get("message_id")
+    if raw_message == "qfnu":
+        await QFNU(websocket, group_id, message_id)
